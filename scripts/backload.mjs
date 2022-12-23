04a10addc28687145e8a0a7a1b641ed41171721e
Edward <edwardxiao29@gmail.com>
2022-12-23 12:27:04 -0500

    backloading DB + json data

---
+++ scripts/backload.mjs
@@ -0,0 +1,45 @@
+import fs from "fs"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+const years = [2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
+
+
+for (let i = 0; i < years.length; i++) {
+
+    let file = fs.readFileSync(`/Users/edwardxiao/Documents/code/t3/salarydashboard/scripts/jsonData/${years[i]}.json`)
+
+    let data = JSON.parse(file)
+
+    for (let j = 0; j < data.length; j++) {
+
+        let dataElement = data[j]
+        console.log(dataElement["Year"])
+        await prisma.salary.create({
+            data: {
+                department : dataElement["Department"],
+                division: dataElement["Division"],
+                title: dataElement["Title"], 
+                salaryAmount: parseFloat(dataElement["Salary"].replace(/,/g, '')),
+                year: dataElement["Year"],
+
+                employee: {
+                    connectOrCreate :{
+                        where: {
+                            name: dataElement["Employee"]
+                        },
+
+                        create: {
+                            name: dataElement["Employee"]
+                        }
+                    }
+                },
+            },
+        })
+    } 
+}
+
+
+
+
